8de7d8dcd0d04fd2fc30ca4fc5283d129f37f350
alilefe <60130500+alilefe@users.noreply.github.com>
2020-07-23 20:30:13 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "StorageAccount-ResourceGroup"
     storage_account_name = "storeterraform98766"
     container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
+    key                  = "prod.terraform.tfstate.new"
   }
 }
